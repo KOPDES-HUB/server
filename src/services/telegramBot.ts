7f31e8b05e25c71124f9f877b832185ec41c755a
tansu317 <c14200090@john.petra.ac.id>
2026-07-11 11:58:45 +0700

    redis off

--- src/services/telegramBot.ts
+++ src/services/telegramBot.ts
@@ -148,7 +148,7 @@ async function handleUpdate(update: any) {
       });
 
       // Clear token
-      await redisConnection.del(redisKey);
+      await redisConnection?.del(redisKey);
 
       await sendBotMessage(
         chatId,
